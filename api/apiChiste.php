6dd0c996e643dd687d8de1a7ce68260c64120101
jaume <jaumecann@gmail.com>
2019-12-19 12:09:09 +0100

    final

--- api/apiChiste.php
+++ api/apiChiste.php
@@ -8,18 +8,19 @@ $sql = "SELECT * FROM chistes ORDER BY RAND() LIMIT 1";
 $result = $mysqli->query($sql);
 
 $numrows = $result->num_rows;
-echo $numrows;
 
 $datos = array();
 
-if($numrows>0){
+if($numrows  >0){
     $datos['type'] = "success";
     $datos['data'] = $result->fetch_all(MYSQLI_ASSOC);
 }
 else
 {
     $datos['type'] = "error";
-    $datos['data'] = 'No hay chistes';
+    $datos['data'] = "No hay chistes";
 }
+
+echo json_encode($datos);
 
 ?>
